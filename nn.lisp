6304463e918d46c95666c6ce4736847e5c031af1
David Selivanov <RTMPDavid@gmail.com>
2014-12-30 17:58:46 +0300

    not quite finished backprop

--- nn.lisp
+++ nn.lisp
@@ -3,20 +3,6 @@
 
 (defun lastcar (l)
   (car (last l)))
-;;layer:
-;;      node_1
-;;      ...
-;;      node_n
-
-;;node:
-;;      weight_input_1 ----summ-func-
-;;      ...              /
-;;      weight_input_n -/
-
-
-;; inputs -> node -> value
-;; (defun calc-node (inputs node)
-;;   (map * ))
 
 (defun make-node (n-outputs &optional (function 'tanh))
   (make-instance 'node
@@ -31,11 +17,47 @@
 	    :initform 'nil
 	    :initarg :weights)))
 
+(defun make-net (&rest layer-spec)
+  (make-instance 'nnet :layers (make-nn layer-spec)))
+				  
 (defgeneric value (node input))
 (defmethod value ((n node) input)
   (let ((in (funcall (node-func n) input)))
     (mapcar (lambda (x) (* x in))
 	    (node-weights n))))
+
+(defclass nnet ()
+  ((layers :initarg :layers
+	   :accessor net-layers)))
+
+(defgeneric activate (nnet inputs))
+
+(defgeneric nth-layer (nnet n))
+
+(defgeneric input-count (nnet))
+(defgeneric node-count (nnet layer))
+(defgeneric layer-count (nnet))
+(defgeneric subnet (nnet start &optional end))
+
+(defmethod activate ((net nnet) inputs)
+  (calc-nn (net-layers net) inputs))
+
+(defmethod nth-layer ((net nnet) n)
+  (nth n (net-layers net)))
+
+(defmethod node-count ((net nnet) layer)
+  (length (nth layer (net-layers net))))
+
+(defmethod input-count ((net nnet))
+  (node-count net 0))
+
+(defmethod layer-count ((net nnet))
+  (length (slot-value net 'layers)))
+
+
+(defmethod subnet ((net nnet) start &optional (end (layer-count net)))
+  (sub-nn (net-layers net) start end))
+
 
 (defun make-input-layer (nodes outputs)
   (loop for i from 1 to nodes appending (list (make-node outputs 'identity))))
@@ -45,35 +67,36 @@
 	appending (list 
 		   (make-node outputs function))))
 
-(defun make-nn (&rest layers)
-  (if (< (length layers) 3) nil
-    (append (list (make-input-layer (car layers) (cadr layers))
-		  (loop with l = (cdr layers)
-			while (not (null (cdr l)))
-			appending (make-layer (car l) (cadr l))
-			do (setf l (cdr l))))
-	    (list (make-layer (lastcar layers) (lastcar layers))))))
-			  
-		    
+(defun make-nn (layers)
+  (if (find 0 layers) (format t "Cannot make network with 0 nodes in a layer.~%")
+    (if (< (length layers) 3) nil
+      (append (list (make-input-layer (car layers) (cadr layers)))
+	      (loop with l = (cdr layers)
+		    while (not (null (cdr l)))
+		    appending (list (make-layer (car l) (cadr l)))
+		    do (setf l (cdr l)))
+	      (list (make-layer (lastcar layers) (lastcar layers)))))))
 
-;	      (list (make-layer (lastcar layers) (lastcar layers) 'identity))))))
-
-(defun calc-layer (layer inputs)
+(defun calc-layer (inputs layer)
   (apply 'mapcar '+
 	 (mapcar 'value
 		 layer 
 		 inputs)))
 
 (defun calc-nn (nn inputs)
-(  (calc-layer (car nn) inputs))
+  (reduce 'calc-layer nn :initial-value inputs))
+  
 (defun sub-nn (nn start &optional (end (length nn)))
   (subseq nn start end))
-
 
 (defun calc-error (outputs target)
   (reduce '+ (mapcar (lambda (ot tg) (expt (- ot tg) 2))
 		     outputs
 		     target)))
 
-(defun activate (nn &rest inputs)
-  (calc-nn nn inputs))
+(defun delta-n (l n delta)
+  (incf (nth n l) delta))
+
+(defun partial-derivatives (nn inputs delta)
+  (loop for i from 0 to (length inputs)
+	
